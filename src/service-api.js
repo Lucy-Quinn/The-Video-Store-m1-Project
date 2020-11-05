07345f132edf4fe10a4d43167801177a5fc1552f
Lucy Quinn <lucy.quinn.uk@gmail.com>
2020-11-05 15:45:04 +0100

    chhange styling to movie rented button. Add setTimeOut and change color.

--- src/service-api.js
+++ src/service-api.js
@@ -1,23 +1,23 @@
 function movieSelector() {
     fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=d0d72ad03cd6e6a02d49e71b43050670')
         .then((response) => {
-            console.log('response', response);
             return response.json();
         })
         .then((data) => {
-
-
             const moviesArray = data.results
-            console.log(data.results);
             moviesArray.forEach((movie) => {
 
                 //create elements
                 const article = document.createElement('article');
                 const overlay = document.createElement('article');
                 const movieContainer = document.createElement('div');
-                // const stars = document.createElement('div');
                 const rentFilm = document.createElement('div');
                 const successRented = document.createElement('div');
+                const filmId = document.getElementById('list-of-films');
+                const successRentedP = document.querySelector('.success-rented');
+
+
+                // const stars = document.createElement('div');
 
 
                 //set class attributes
@@ -26,29 +26,19 @@ function movieSelector() {
                 overlay.setAttribute('class', 'text-overlay');
                 rentFilm.setAttribute('class', 'rent-film');
 
+
                 //amending html/styling
                 article.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500${movie.poster_path}')`;
 
                 rentFilm.innerHTML = `
-                <button>Rent Movie</button>
+                <button id="rent-button">Rent Movie</button>
                  <i class="fas fa-heart"></i>
-
                 `
+
                 successRented.innerHTML = `
                     <p class="success-rented">Enjoy your movie!</p>
                     <br>
                 `
-
-
-                // stars.innerHTML = `
-                // <section id="all-stars">
-                // <i id="1" class="stars fas fa-star"></i>
-                // <i id="2" class="stars fas fa-star"></i>
-                // <i id="3" class="stars fas fa-star"></i>
-                // <i id="4" class="stars fas fa-star"></i>
-                // <i id="5" class="stars fas fa-star"></i>
-                // </section> 
-                // `
                 overlay.innerHTML = `
                 <h2>${movie.title}</h2>
                 <br>
@@ -61,37 +51,59 @@ function movieSelector() {
                 `
 
 
-                //append elements to
+                // stars.innerHTML = `
+                // <section id="all-stars">
+                // <i id="1" class="stars fas fa-star"></i>
+                // <i id="2" class="stars fas fa-star"></i>
+                // <i id="3" class="stars fas fa-star"></i>
+                // <i id="4" class="stars fas fa-star"></i>
+                // <i id="5" class="stars fas fa-star"></i>
+                // </section> 
+                // `
+
+
+
+                //append elements to movieContainer
                 movieContainer.appendChild(article)
                 movieContainer.appendChild(overlay)
                 movieContainer.appendChild(rentFilm)
                 // movieContainer.appendChild(stars)
-
-
-                //append movieContainer to list-of-films
-                const filmId = document.getElementById('list-of-films');
                 filmId.appendChild(movieContainer)
-                // const articles = document.querySelectorAll('.movie-card')
-                // const overlays = document.querySelectorAll('.text-overlay')
 
-                // const article = document.querySelector('.movie-card');
-                // const overlay = document.querySelector('.text-overlay')
-
-                // article.forEach((article) => {
+                // Event listeners and functions for the movie-card and over-lay
                 article.addEventListener('click', function () {
                     overlay.classList.toggle('text-show');
-
                 })
                 overlay.addEventListener('click', function () {
                     overlay.classList.toggle('text-show');
+                })
 
+                //Event listener and function for heart toggle
+                const hearts = movieContainer.querySelectorAll('.fa-heart')
+                hearts.forEach((heart) => {
+                    heart.addEventListener('click', function (event) {
+                        const click = event.target
+                        click.classList.toggle('heart-full')
+                    })
+                });
+
+                // Event listener and function for success message on renting movie
+                const rentButtons = rentFilm.querySelectorAll('#rent-button');
+                rentButtons.forEach((rentButton) => {
+                    rentButton.addEventListener('click', function (event) {
+                        let button = event.target
+                        rentFilm.appendChild(successRented)
+                        setTimeout(function () {
+                            rentFilm.removeChild(successRented)
+
+                            button.style.backgroundColor = 'red';
+                        }, 2000);
+                        button.innerHTML = 'Movie Rented'
+                    })
                 })
 
 
-                // Click 'Rent Movie' to show success message
-                rentFilm.addEventListener('click', function () {
-                    rentFilm.appendChild(successRented)
-                })
+
 
                 // stars.addEventListener('click', function (event) {
                 //     const targettedStar = event.target;
@@ -115,26 +127,25 @@ function movieSelector() {
                 //     // stars.classList.toggle("stars-full")
                 // });
 
-                const hearts = movieContainer.querySelectorAll('.fa-heart')
-                hearts.forEach((heart) => { heart.addEventListener('click', heartClick) });
+
             })
-            // console.log('data', data);
         })
 }
 movieSelector();
 
+
 //Heart function
-function heartClick(event) {
-    const click = event.target
-    click.classList.toggle('heart-full')
-}
+// function heartClick(event) {
+//     const click = event.target
+//     console.log(click);
+//     click.classList.toggle('heart-full')
+// }
 
 //Stars function
 function changeStars(event) {
     const targettedStar = event.target;
     const starId = parseInt(targettedStar.id);
     let starStringId = '';
-    console.log(targettedStar);
     for (let i = 1; i <= starId; i++) {
         starStringId = i;
         let turnGold = document.getElementById(`${starStringId}`);
@@ -144,14 +155,12 @@ function changeStars(event) {
     for (let i = 5; i >= starId; i--) {
         starStringId = i;
         let turnGold = document.getElementById(`${starStringId}`);
-        console.log('turngold', turnGold);
         turnGold.style.color = 'white';
     }
     // stars.querySelector('.stars')
     // console.log(stars);
     // // stars.setAttribute('class', 'stars')
     // stars.classList.toggle("stars-full")
-
 }
 
 //Get genre function
@@ -205,6 +214,9 @@ function getGenre(genreArr) {
     })
     return finalGenre.slice(0, -2)
 }
+
+
+
 
 
 
